5964865fca0f4c5f0f3a8bec3993881728b578c3
vishnudivya2020 <vishnudivyabharathi@gmail.com>
2025-07-14 14:03:38 +0530

    Add new changes

--- server/index.js
+++ server/index.js
@@ -12,9 +12,10 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors());
 
 app.use(cors({
-  origin: 'https://dashing-croissant-83db96.netlify.app',
-  credentials: true,
+  origin: 'https://guileless-buttercream-04252e.netlify.app',
+  credentials: true
 }));
+
 
 
 app.use("/user", userroutes);
